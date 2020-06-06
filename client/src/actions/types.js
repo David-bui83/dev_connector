054121e5aefd13ebfc84911a5534a3710c60759f
Mona Lisa <david.t.bui83@gmail.com>
2020-06-05 18:54:04 -0700

    dont with load user and set auth token

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,9 @@
+// Alert types
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+// Register types
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+// User loaded and auth eror types
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
