3b9447e400e280b5a8b10af57c0a840c50455ddd
Mona Lisa <david.t.bui83@gmail.com>
2020-06-08 21:37:35 -0700

    fixed some bugs

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,17 +8,20 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to="/profiles">Developers</Link>
+      </li>
+      <li>
         <Link to="/dashboard"><i className="fas fa-user"></i>{' '}<span className="hide-sm">Dashboard</span></Link>
-        </li>
+      </li>
       <li>
         <a onClick={logout} href="#!"><i className="fas fa-sign-out-alt"></i>{' '} <span className="hide-sm">Logout</span></a>
-        </li>
+      </li>
     </ul>
   );
 
   const guestLinks = (
     <ul>
-        <li><a href="#!">Developers</a></li>
+        <li><Link to="/profiles">Developers</Link></li>
         <li><Link to="/register">Register</Link></li>
         <li><Link to="/login">Login</Link></li>
       </ul>
