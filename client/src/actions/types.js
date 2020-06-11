939604276bc015f33abe00f4be22994f3d1a29b0
Mona Lisa <david.t.bui83@gmail.com>
2020-06-10 20:29:17 -0700

    done with single post view

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -21,6 +21,7 @@ export const PROFILE_ERROR = 'PROFILE_ERROR';
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 // Post types
 export const GET_POSTS = 'GET_POSTS';
+export const GET_POST = 'GET_POST';
 export const POST_ERROR = 'POST_ERROR';
 // Likes and Unlikes types
 export const UPDATE_LIKES = 'UPDATE_LIKES';
